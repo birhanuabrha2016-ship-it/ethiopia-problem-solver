3ddecc2f98d88fc2a5aeb0f680c607da2b96b6b3
birhanuabrha2016-ship-it <birhanuabrha2016@gmail.com>
2026-02-18 18:04:23 +0300

    Update APP_URL to production frontend URL

--- frontend/src/config.prod.js
+++ frontend/src/config.prod.js
@@ -1,6 +1,6 @@
 const config = {
   API_URL: 'https://ethiopia-problem-solver-backend.onrender.com',
-  APP_URL: 'https://your-frontend-url.netlify.app' // We'll update this after frontend deployment
+  APP_URL: 'https://ethiopia-problem-solver.netlify.app'
 };
 
 export default config;
