389a0992569601b21182a41882bd3e6a64c5e953
birhanuabrha2016-ship-it <birhanuabrha2016@gmail.com>
2026-02-19 15:50:23 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,12 +1,15 @@
 // config.js - THIS SMARTLY CHOOSES THE RIGHT SETTINGS
+import configDev from './config.dev';
+import configProd from './config.prod';
+
 let config;
 
 if (process.env.NODE_ENV === 'production') {
   // If we are on the live server, use the production settings
-  config = require('./config.prod').default;
+  config = configProd;
 } else {
   // If we are on our computer, use the development settings
-  config = require('./config.dev').default;
+  config = configDev;
 }
 
 export default config;
