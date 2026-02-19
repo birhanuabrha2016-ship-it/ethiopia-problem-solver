b6dc477617cc6a783784218afa37b02cf30d271b
birhanuabrha2016-ship-it <birhanuabrha2016@gmail.com>
2026-02-19 15:42:38 +0300

    Update API and APP URLs to use localhost

--- frontend/src/config.dev.js
+++ frontend/src/config.dev.js
@@ -1,7 +1,7 @@
 // config.dev.js - THIS IS FOR LOCAL TESTING
 const config = {
-  API_URL: 'http://192.168.137.128:5000',
-  APP_URL: 'http://192.168.137.128:3000'
+  API_URL: 'http://localhost:5000',
+  APP_URL: 'http://localhost:3000'
 };
 
 export default config;
